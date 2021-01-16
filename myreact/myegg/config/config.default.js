98ba0177a5ebc0343fccc4eb692c1b5414d3c882
celiaws <1727804796@qq.com>
2021-01-16 18:48:41 +0800

    sss

--- myreact/myegg/config/config.default.js
+++ myreact/myegg/config/config.default.js
@@ -10,49 +10,60 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {
-
-  };
+  const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1610017555884_8741';
+  config.keys = appInfo.name + '_1606300057304_4405';
 
-  config.cors = {
-    origin: 'http://192.168.2.114:3000',
+
+  //关闭csrf
+  config.security = {
+    csrf: {
+      enable: false
+    }
+  }
+  // 允许上传文件
+  config.multipart = {
+    mode: 'file',
+    ignoreJSON:true,
   };
+
+  // 跨域
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+}
+ 
+  // 配置数据库
+  config.mysql={
+    client:{
+      //host
+      host:"localhost",
+      //端口
+      port:"3306",
+      //用户名
+      user:"root",
+      //密码
+      password:"root",
+      //数据库名
+      database:"h200801"   
+    }
+  }
+
+   // 配置缓存
+   config.session = {
+    key: 'HQYJ_SSION',
+    maxAge: 24 * 3600 * 1000, // 1 天
+    httpOnly: true,
+    encrypt: true,
+  };
+
   // add your middleware config here
   config.middleware = [];
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
-  };
-  
-  config.mysql={
-  client:{
-  host:'localhost',
-  port:'3306',
-  user:'root',
-  password:'root',
-  database:'xiaohong'
-  }
-  };
-
-  config.security={
-    csrf:{
-      enable:false
-    }
-  };
-
-  config.multipart = {
-  mode: 'file',
-   };
-
-  config.session = {
-    key: 'MY_SSION',
-    maxAge: 24 * 3600 * 1000, // 1 天
-    httpOnly: true,
-    encrypt: true,
   };
 
   return {
